6f48bbceaaba088e007b89dc25dc24ab489b626d
K Dhawal Verma <dhawalverma2808@gmail.com>
2022-08-27 23:51:16 +0530

    First Upload

---
+++ port.js
@@ -0,0 +1,8 @@
+const themeBtn = document.querySelector('.theme-btn');
+
+themeBtn.addEventListener("click",function(){
+    var element = document.body;
+    element.classList.toggle("light-mode");
+    element.classList.toggle()
+})
+
